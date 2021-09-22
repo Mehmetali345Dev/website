e341524d8d787696ef6bb8ef75aca7f4fb391e11
Mehmet Ali Külahçı <mehmetali345@mehmetali345.xyz>
2021-09-23 00:01:49 +0300

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dark:(bg-gray-900 text-gray-50) bg-gray-200 flex flex-col items-center min-h-[100vh]">
-    <div class="w-full">
+    <div class="w-full sticky top-0">
       <NavBlog
         v-if="$route.path.includes('blog')"
         class="sticky top-0 animated z-40 animate-slide-in-down"
