cf6146f9214fe347d6dc3e4b8575fb52a57532cb
Mehmetali345Dev <345m.ali2007@gmail.com>
2021-12-05 17:42:13 +0300

    refactor: tag style

--- src/pages/blog/_slug.vue
+++ src/pages/blog/_slug.vue
@@ -57,10 +57,10 @@
     </div>
     <div class="w-full mt-4" v-if="getTags.length > 0">
       <h1 class="text-2xl font-bold">Etiketler</h1>
-      <div class="grid grid-cols-3 mt-2 gap-3">
+      <div class="grid grid-cols-3 mt-2 gap-3 md:flex md:space-x-3">
         <div
           class="bg-gray-900 flex justify-center items-center rounded-md p-4 bg-opacity-30"
-          v-for="{ tag, index } in getTags"
+          v-for="(tag, index) in getTags"
           :key="`tag-${index}`"
         >
           {{ tag }}
