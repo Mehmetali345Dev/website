fec08d1a71e708bf28a615ab0e45c73e7a276b21
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2021-12-13 22:04:34 +0300

    feat: better index page with links

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full flex flex-col items-start space-y-2">
+  <header class="w-full flex gap-2 flex-col items-start">
     <div class="items-start">
       <div class="grid items-center about mt-2">
         <h1 class="font-bold text-2xl text-2xl">Hello, I'm Mehmet Ali</h1>
@@ -9,14 +9,48 @@
         </p>
       </div>
     </div>
-    <WidgetDiscord />
-    <WidgetSpotify />
-    <WidgetContact />
+    <div class="flex flex-col gap-1 w-full">
+      <WidgetDiscord />
+      <WidgetSpotify />
+    </div>
+    <div class="grid md:grid-cols-3 gap-2">
+      <nuxt-link
+        v-for="(link, index) in links"
+        :key="`link-${index}`"
+        :to="link"
+        class="bg-gray-900 w-full gap-2 flex flex-col bg-opacity-30 p-4"
+      >
+        <h1 class="text-xl font-bold">{{ link.name }}</h1>
+        <p class="text-lg text-gray-200">{{link.desc}}</p>
+      </nuxt-link>
+    </div>
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'My Projects',
+          to: '/projects',
+          desc: "You can see my projects and works from here."
+        },
+        {
+          name: 'Donate Me',
+          to: '/donate',
+          desc: "You can donate to me if you want."
+        },
+        {
+          name: 'Turkish Blog',
+          to: '/blog',
+          desc: "You can see my Turkish blog from here."
+        },
+      ],
+    }
+  }
+}
 </script>
 
 <style></style>
