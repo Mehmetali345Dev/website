3d81c0c0479a9626c732ba3ae4fa4af86d2ef6b0
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2021-12-19 17:21:52 +0300

    refactor: better technologies card

--- src/components/Widget/Technologies.vue
+++ src/components/Widget/Technologies.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="flex flex-col">
     <h1 class="font-bold text-lg">Technologies I use</h1>
-    <div class="h-full my-2 grid grid-cols-3 md:grid-cols-6  gap-3">
+    <div class="h-full my-2 grid sm:grid-cols-4 gap-3">
       <div
         v-for="(tech, index) in technologies"
         :key="`tech-${index}`"
-        class="
-          bg-gray-800
-          rounded-md
-          bg-opacity-30
-          flex flex-col
-          items-center
-          justify-center
-          p-2
-          space-y-2
-        "
+        class="bg-gray-900 rounded-md bg-opacity-30 flex items-center justify-between p-2"
       >
         <IconBrand :name="tech" class="w-10 h-10" />
         <h1 class="bold">{{ tech }}</h1>
       </div>
     </div>
-    <nuxt-link to="/projects" class="bg-green-600 font-bold p-4 rounded-md">
-      See my all works, projects and repos
-    </nuxt-link>
+    <nuxt-link
+      to="/projects"
+      class="bg-green-600 font-bold p-4 rounded-md"
+    >See my all works, projects and repos</nuxt-link>
   </div>
 </template>
 
