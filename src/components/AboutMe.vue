8608ed28939c39cf448f72c19f149ea140f11ca1
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-02-27 00:08:18 +0300

    feat: few additions

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -8,15 +8,31 @@
           programs and web applications using Vue.js, JavaScript and C#.
         </p>
       </div>
-      <nuxt-img
-        src="/icon.png"
-        class="rounded-md w-32 h-32"
-        alt="Mehmetali345Dev"
-      />
+      <nuxt-img src="/icon.png" class="rounded-md w-32 h-32" alt="Mehmetali345Dev" />
     </div>
     <div class="flex flex-col md:(grid-cols-2 grid) gap-2 w-full">
       <WidgetDiscord />
       <WidgetSpotify class="h-full" />
+    </div>
+    <div class="grid md:grid-cols-2 gap-2">
+      <div class="flex flex-col gap-2">
+        <h1 class="font-bold text-lg">Latest Blog Posts</h1>
+        <CardPost
+          type="rest"
+          v-for="(post, index) in posts"
+          :key="`latest-${index}`"
+          :post="post"
+          class="w-full h-full"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <h1 class="font-bold text-lg">My Projects</h1>
+        <CardProject
+          :project="project"
+          v-for="(project, index) in projects"
+          :key="`project-${index}`"
+        />
+      </div>
     </div>
     <h1 class="text-2xl font-bold">About Me</h1>
     <div class="grid w-full md:grid-cols-2 gap-2">
@@ -26,16 +42,9 @@
         :to="link.to"
         class="bg-gray-900 scale-up rounded-md w-full gap-2 flex flex-col bg-opacity-30 p-4"
       >
-        <h1 class="text-xl items-center font-bold truncate">
-          {{ link.name }}
-        </h1>
-        <p class="text-lg text-gray-800 dark:text-gray-200 line-clamp-2">
-          {{ link.desc }}
-        </p>
-        <p
-          v-if="link.featured"
-          class="gap-1 items-center font-bold text-lg flex"
-        >
+        <h1 class="text-xl items-center font-bold truncate">{{ link.name }}</h1>
+        <p class="text-lg text-gray-800 dark:text-gray-200 line-clamp-2">{{ link.desc }}</p>
+        <p v-if="link.featured" class="gap-1 items-center font-bold text-lg flex">
           Featured
           <IconFire class="w-6 rounded-full h-6 text-red-600" />
         </p>
@@ -49,16 +58,9 @@
         :to="link.to"
         class="bg-gray-900 scale-up rounded-md w-full gap-2 flex flex-col bg-opacity-30 p-4"
       >
-        <h1 class="text-xl items-center font-bold truncate">
-          {{ link.name }}
-        </h1>
-        <p class="text-lg text-gray-800 dark:text-gray-200 line-clamp-2">
-          {{ link.desc }}
-        </p>
-        <p
-          v-if="link.featured"
-          class="gap-1 items-center font-bold text-lg flex"
-        >
+        <h1 class="text-xl items-center font-bold truncate">{{ link.name }}</h1>
+        <p class="text-lg text-gray-800 dark:text-gray-200 line-clamp-2">{{ link.desc }}</p>
+        <p v-if="link.featured" class="gap-1 items-center font-bold text-lg flex">
           Featured
           <IconFire class="w-6 rounded-full h-6 text-red-600" />
         </p>
@@ -71,6 +73,19 @@
 export default {
   data() {
     return {
+      projects: [
+        {
+          title: 'This website',
+          description: 'My personal portfolio made with NuxtJS and WindiCSS',
+          link: '/',
+        },
+        {
+          title: 'MADE',
+          description:
+            "Awesome showcase for dotfiles and a wiki for ricing and for intruducing WM/DE's and ricing releated software to new users.",
+          link: 'https://made.345dev.me',
+        },
+      ],
       links: {
         me: [
           {
@@ -116,8 +131,17 @@ export default {
           },
         ],
       },
+      posts: []
     }
   },
+  async fetch() {
+    const latestPosts = await this.$content()
+      .sortBy('date', 'desc')
+      .limit(2)
+      .without(['body'])
+      .fetch()
+    this.posts = latestPosts
+  }
 }
 </script>
 
